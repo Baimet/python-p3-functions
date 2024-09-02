ff09cbcb8069d03d85763c04529a8e1a9ea211d7
baimet <isaacbaimetyatich@gmail.com>
2024-09-02 11:25:33 +0300

    Greet Programmer

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    greet_programmer="Hello, programmer!"
+    print(greet_programmer)
 
-def greet(name):
-    pass
+greet_programmer()
+
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
+
+greet()
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+greet_with_default("Sunny")
 
-def halve(number):
-    pass
+def add(num1=1, num2=2):
+    return num1 + num2
+add()
+
+def halve(number=4):
+    return number/2
+halve()
